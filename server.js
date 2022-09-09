dfe4c2ca000e5ffa82dee9eaa8aeb84e53b75be0
Jason <dljvaz@gmail.com>
2022-09-08 23:01:09 -0500

    cors header fix

--- server.js
+++ server.js
@@ -26,18 +26,14 @@ const app = express();
 app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
   res.header("Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+
   next();
 });
 // app.use(express.json({ strict: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/tmp',express.static(path.join(__dirname, "tmp")));
+app.use("/tmp", express.static(path.join(__dirname, "tmp")));
 // app.use(
 //   cors({
 //     origin: 'http://localhost:4200',
